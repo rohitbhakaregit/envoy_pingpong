b9ea6a1c9af78647bc0b197a140f73e458864764
rohitbhakaregit <bhakarerohit@gmail.com>
2020-04-10 18:44:36 +0530

    remove unwanted files

--- t.sh
+++
@@ -1,6 +0,0 @@
-echo "# envoy_pingpong" >> README.md
-git init
-git add .
-git commit -m "first commit"
-git remote add origin https://github.com/rohitbhakaregit/envoy_pingpong.git
-git push -u origin master
